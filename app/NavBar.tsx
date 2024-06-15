6a9ef8ac61c98de15fe2d9bbf356951753c69f5c
hkshakib <humayunkibriashakib@gmail.com>
2024-06-15 12:43:16 +0600

    style: style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import React from "react";
 import { GiBugNet } from "react-icons/gi";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -16,7 +22,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-gray-500 hover:text-black transition-colors"
+            className={classNames({
+              'text-gray-400': link.href !== currentPath,
+              'text-black': link.href == currentPath,
+              'hover:text-black transition-colors': true,
+            })}
             href={link.href}
           >
             {link.label}
