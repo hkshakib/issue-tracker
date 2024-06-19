03af84bdcba5131d20627c60228f55509e94564c
hkshakib <humayunkibriashakib@gmail.com>
2024-06-19 10:33:22 +0600

    feat: update issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,10 +7,9 @@ import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import dynamic from "next/dynamic";
 
-const SimpleMDE = dynamic(
-  ()=>import('react-simplemde-editor'), 
-  {ssr: false}
-);
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,8 +21,7 @@ import { Issue } from "@prisma/client";
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
-
-const IssueForm = ({issue}: {issue?: Issue}) => {
+const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
   const {
     register,
@@ -39,7 +37,8 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -73,7 +72,8 @@ const IssueForm = ({issue}: {issue?: Issue}) => {
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner />}{" "}
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}{" "}
         </Button>
       </form>
     </div>
