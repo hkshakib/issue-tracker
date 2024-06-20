f4fab31de03c9290839cd87cc591e745a811625d
hkshakib <humayunkibriashakib@gmail.com>
2024-06-20 12:33:26 +0600

    fix: Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -5,19 +5,15 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import dynamic from "next/dynamic";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+import SimpleMDE from "react-simplemde-editor";
 
 import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
-import { z } from "zod";
 import { Issue } from "@prisma/client";
+import { z } from "zod";
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,15 +1,2 @@
-import { Box } from '@radix-ui/themes';
-import React from 'react'
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-
-const LoadingNewIssuePage = () => {
-  return (
-    <Box>
-      <Skeleton />
-      <Skeleton height='20rem'/>
-    </Box>
-  )
-}
-
-export default LoadingNewIssuePage;
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
